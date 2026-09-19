73c7d5f0124db1ca22ab6be4da7073d9846926a9
suraj231100 <2022wa86068@wilp.bits-pilani.ac.in>
2026-09-20 00:20:25 +0530

    Update output message in App.java

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Lab - Jenkins Maven Build");
+        System.out.println("DevOps Lab - Jenkins Maven Build - 2022WA86068");
     }
 }
